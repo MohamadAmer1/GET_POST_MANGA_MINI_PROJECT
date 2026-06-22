a416ef6e41d49d327d675e786df771255a9be734
Mohamad Amer <mohmdsosolove@gmail.com>
2026-06-22 12:31:52 +0300

    Added POST Method in MiniProject

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const chapterNumber = document.getElementById("chapterNumber");
 const statusCompleted = document.getElementById("statusCompleted");
 const mangaContainer = document.getElementById("mangaContainer");
 const loadingMessage = document.getElementById("loadingMessage");
+const mangaForm = document.getElementById("mangaForm");
 
 async function getManga() {
   try {
@@ -15,37 +16,65 @@ async function getManga() {
       throw new Error("An Error Has Been Detected");
     }
     const manga = await res.json();
-    console.log(manga);
 
-    getData(manga);
+    displayData(manga);
   } catch (error) {
     console.log(error);
   }
 }
 
-function getData(mangas) {
+function displayData(mangas) {
   if (mangas.length === 0) {
     loadingMessage.textContent = "Type in a Manga!";
     loadingMessage.style.color = " red";
     return;
   }
   loadingMessage.textContent = "";
-  mangaContainer.innerHTML = "";
 
   mangas.forEach((manga) => {
-    mangaContainer.innerHTML += `
-    <div class="manga-card">
-    <img src="${manga.image}">
-    <div class="manga-info">
-        <h3>${manga.name}</h2>
-        <p>Chapters: <b>${manga.chapterNumber}</b></p>
-        <p>Status-Completed: <b>${manga.statusCompleted}</b></p>
-        <p>Price: <b>$${manga.price}</b></p>
-    </div>
-
-    </div>
-    `;
+    const card = document.createElement("div");
+    card.classList.add("manga-card");
+    card.innerHTML = `
+      <img src="${manga.image}">
+      <div class="manga-info">
+          <h3>${manga.name}</h2>
+          <p>Chapters: <b>${manga.chapterNumber}</b></p>
+          <p>Status-Completed: <b>${manga.statusCompleted}</b></p>
+          <p>Price: <b>$${manga.price} per-chapter</b></p>
+      </div>`;
+    mangaContainer.appendChild(card);
   });
 }
 
 getManga();
+async function sendManga(e) {
+  e.preventDefault();
+  try {
+    const newManga = {
+      name: mangaName.value,
+      image: mangaImg.value,
+      price: Number(mangaPrice.value),
+      chapterNumber: Number(chapterNumber.value),
+      statusCompleted: statusCompleted.value,
+    };
+    if (statusCompleted.value === "false") {
+      newManga.statusCompleted = false;
+    } else {
+      newManga.statusCompleted = true;
+    }
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newManga),
+    });
+    const createdManga = await response.json();
+    console.log(createdManga);
+    displayData([newManga]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+mangaForm.addEventListener("submit", sendManga);
